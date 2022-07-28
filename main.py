6c40f12c65f24674a21aa0d1f953a2921bcfcaf9
Ridam Shrestha <ridamshrestha42@gmail.com>
2022-07-28 17:51:54 +0545

    Auto Playlist Detection Added

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+# YT_DOWNLOADER V 1.0
+# github.com/ridam9999/YT_DOWNLOADER
+
 from pytube import YouTube, Playlist
 import os
 
-
 run = True
-sd = '480'
-hd = '720'
-fhd = '1080'
 internal_storage_music = '/home/jarvis/Music'
 external_storage_music = '/media/jarvis/UNDERLOAD/Music'
 internal_storage_video = '/home/jarvis/Videos'
 external_storage_video = '/media/jarvis/UNDERLOAD/Video'
-def download_song(option):
-    if option == 's':
+
+def download_song():
+    yt = None
+    playlist = None
+    url = str(input('URL: '))
+    destination = str(input('PATH: '))
+    try:
+        yt = YouTube(url)
+    except:
+        playlist = Playlist(url)
+    if yt:
         try:
-            yt = YouTube(str(input('URL: ')))
             song = yt.streams.filter(only_audio=True).first()
-            destination = str(input('PATH: '))
             print('')
             if not(os.path.exists(destination)):
                 if destination == 'i':
@@ -23,56 +29,62 @@ def download_song(option):
                 elif destination == 'e':
                     destination = external_storage_music
                 else:
-                    print('DIRECTORY INVALID \n')
-                    print('COPIED TO DEFAULT SONG DIRECTORY \n')
+                    print('DIRECTORY INVALID --------> COPIED TO DEFAULT SONG DIRECTORY')
                     destination = internal_storage_music
             song.download(output_path = destination)
             print(yt.title + " has been successfully downloaded.")
         except:
-            print('CONNECTION ERROR') 
-    elif option == 'p':
+            print('CONNECTION ERROR')
+    elif playlist:
         try:
-            playlist = Playlist(str(input('URL: ')))
-            destination = str(input('PATH: '))
             for i in range(len(playlist.videos)):
                 yt = YouTube(playlist.video_urls[i])
                 song = yt.streams.filter(only_audio=True).first()
                 if not(os.path.exists(destination)):
-                    print('DIRECTORY INVALID \n')
-                    print('COPIED TO DEFAULT SONG DIRECTORY \n')
+                    print('DIRECTORY INVALID --------> COPIED TO DEFAULT SONG DIRECTORY')
                     destination = '/home/jarvis/Music'
                 song.download(output_path = destination)
                 print(yt.title + " has been successfully downloaded.") 
         except:
             print('CONNECTION ERROR') 
 
-
 def download_video():
-    yt = YouTube(str(input('URL: ')))
+    quality = ['360p', '480p', '720p', '1080p']
+    yt, playlist = None, None
+    url = str(input('URL: '))
     destination = str(input('PATH: '))
-    print('')
+    try:
+        yt = YouTube(url)
+    except:
+        playlist = Playlist(url)
     if not(os.path.exists(destination)):
         if destination == 'i':
             destination = internal_storage_video
         elif destination == 'e':
             destination = external_storage_video
         else:
-            print('DIRECTORY INVALID \n')
-            print('COPIED TO DEFAULT SONG DIRECTORY \n')
-            destination = internal_storage_music
-        #quality = input('1.SD   2.HD    3.FHD : ')
-        yt.streams.filter(res='720p').first().download(destination)
+            print('DIRECTORY INVALID --------> COPIED TO DEFAULT VIDEO DIRECTORY')
+            destination = internal_storage_video
+    try:
+        quality_wanted = int(input('1.SD   2.HD    3.FHD : '))
+        quality[quality_wanted]
+        print(quality[quality_wanted])
+    except:
+        quality_wanted = 2
+    if yt:
+        yt.streams.filter(res=quality[quality_wanted]).first().download(destination)
         print(yt.title + " has been successfully downloaded.") 
+    elif playlist:
+        for i in range(len(playlist.videos)):  
+            playlist.videos[i].streams.filter(res=quality[quality_wanted]).first().download(destination)
+            print(playlist.videos[i].title + " has been successfully downloaded.")
+    else:
+        print('INVALID URL')
 
 while run:
-    option = input('DOWNLOAD SONG(s) / VIDEO(v)? ')
+    option = input('DOWNLOAD SONG(s) / VIDEO(v)? ') 
     if option == 's':
-        option = input('PLAYLIST(p) / SINGLE(s)')
-        if option == 's' or option == 'p':
-            download_song(option)
-        else:
-            print('WRONG INPUT DETECTED!!')
-        
+        download_song()
     elif option == 'v':
         download_video()
     else:
